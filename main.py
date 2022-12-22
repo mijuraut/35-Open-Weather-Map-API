26c06b9f5665bce9f8c406f1ad50fde26667c692
Mikko <mijuraut@gmail.com>
2022-12-22 09:33:54 +0200

    Some response  /  request stuff.

--- main.py
+++ main.py
@@ -1,5 +1,27 @@
+import requests
+
+OMW_Endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 api_key = "936861fbc39ce1f6bb0e1cea570af802"
 
-print(api_key)
+# ?lat={61.058449}&lon={28.186991}&appid=936861fbc39ce1f6bb0e1cea570af802
 
-print(api_key)
+weather_params = {
+    "lat": 61.058449,
+    "lon": 28.186991,
+    "appid": api_key
+}
+
+response = requests.get(OMW_Endpoint, params=weather_params)
+# response.raise_for_status()
+print(response.status_code)
+
+# data = response.json()
+# weather_data = data["results"]
+
+# print(weather_data)
+
+# One Call API
+
+# Lappeenranta:
+# lat = 61.058449
+# lng = 28.186991
